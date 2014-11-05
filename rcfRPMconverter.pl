dfb78061573dd0840b8f6342b14ab8e9e9b7f1b4
Amory Meltzer <Amorymeltzer@gmail.com>
2014-11-05 13:53:39 -0800

    Allow rcf calculation and inputs

--- rcfRPMconverter.pl
+++ rcfRPMconverter.pl
@@ -5,6 +5,12 @@
 ## Built-in defaults for common fuges
 ## Handle through interactive
 
+# g = (1.118 x 10^-5) x R x S^2
+# g = g
+# R = radius, cm
+# S = rpm
+
+
 use strict;
 use warnings;
 use diagnostics;
@@ -14,14 +20,30 @@ use Getopt::Std;
 
 # Parse commandline options
 my %opts = ();
-getopts('h', \%opts);
-if( $opts{h} ) { usage(); exit; } # Usage
+getopts('r:g:c:h', \%opts);
 
+if ($opts{h}) {
+  usage(); exit;
+}
 
+if ($opts{r} && $opts{g}) {
+  print "You may only specificy either rpm (r) or rcf/g (g)\n";
+  exit;
+}
 
+my $c;
+if ($opts{c}) {
+  $c = $opts{c};
+}
+else {
+  print "What is the radius of your centrifuge (cm)?\n";
+  $c = <>;
+}
 
-
-
+if ($opts{r}) {
+  my $g = 1.118/100000*$c*$opts{r}*$opts{r};
+  print "\n$g\n";
+}
 
 
 
@@ -36,7 +58,7 @@ if( $opts{h} ) { usage(); exit; } # Usage
 # Escapes not necessary but ensure pretty colors
 # Final line must be unindented?
 sub usage
-{
+  {
     print <<USAGE;
 Usage: $0 [-abfht -d <month[1-12]/day[1-31]>]
       -a Display all times (equivalent to -bf)
@@ -48,4 +70,4 @@ Usage: $0 [-abfht -d <month[1-12]/day[1-31]>]
       -c Turn off color printing
       -H print this message
 USAGE
-}
+  }
