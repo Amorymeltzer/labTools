e9b255155a8ee91366466a0d3b1eb1a1bff23f9d
Amory Meltzer <Amorymeltzer@gmail.com>
2014-11-05 13:10:08 -0800

    Todo list

--- rcfRPMconverter.pl
+++ rcfRPMconverter.pl
@@ -1,6 +1,9 @@
 #!/usr/bin/env perl
 # rcfRPMconverter.pl by Amory Meltzer
 # Convert to and from rcf (g-force) and rpm on centrifuges
+## Optional option to select from/to
+## Built-in defaults for common fuges
+## Handle through interactive
 
 use strict;
 use warnings;
