03608e5bd57ee981a860f792b58483c8914872e8
Amory Meltzer <Amorymeltzer@gmail.com>
2014-11-22 22:52:59 -0800

    Mogrify, really

--- tiff-to-jpg.sh
+++ tiff-to-jpg.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # tiff-to-jpg.sh by Amory Meltzer
 # Easily bastch-convert tiff files to jpegs
-# Just a simple wrapper around imagemagick's convert:
+# Just a simple wrapper around imagemagick's mogrify:
 # http://www.imagemagick.org/script/convert.php
