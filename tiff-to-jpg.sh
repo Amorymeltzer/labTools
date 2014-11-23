11db5ca6c186949354f3deaffe09c84c65a2ea19
Amory Meltzer <Amorymeltzer@gmail.com>
2014-11-22 22:35:07 -0800

    Begin tiff to jpg convert, just a wrapper around imagemagick's convert: http://www.imagemagick.org/script/convert.php

---
+++ tiff-to-jpg.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# tiff-to-jpg.sh by Amory Meltzer
+# Easily bastch-convert tiff files to jpegs
+# Just a simple wrapper around imagemagick's convert:
+# http://www.imagemagick.org/script/convert.php
