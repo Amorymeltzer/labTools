c665e46efa09e854d87193a3ec110a2e8c21a232
Amory Meltzer <Amorymeltzer@gmail.com>
2014-11-22 22:55:31 -0800

    Get path

--- tiff-to-jpg.sh
+++ tiff-to-jpg.sh
@@ -3,3 +3,10 @@
 # Easily bastch-convert tiff files to jpegs
 # Just a simple wrapper around imagemagick's mogrify:
 # http://www.imagemagick.org/script/convert.php
+
+if [ $1 ]; then
+    mogpath=$1
+    echo $mogpath
+else
+    echo "Please specify a directory"
+    fi
